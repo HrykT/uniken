1a871db07b4e9bf65b6c0f534451351bd069f0c4
N0203 <N0203>
2016-10-20 09:10:21 +0900

    正規化するか選択可能とした

--- MyPCA.py
+++ MyPCA.py
@@ -7,17 +7,19 @@ Created on Mon Sep 26 16:12:04 2016
 
 class MyPCA:
     #主成分分析のためのクラス
-    def __init__(self, target):
-        #初期化時にtarget（元データ）を正規化する
-        from sklearn.preprocessing import StandardScaler
-        std = StandardScaler()
-        self.std_data = std.fit_transform(target)
+    def __init__(self, target, standard=True):
+        self.base = target
+        if standard:
+            #初期化時にtarget（元データ）を正規化する
+            from sklearn.preprocessing import StandardScaler
+            std = StandardScaler()
+            self.std = std.fit_transform(self.base)
         #pcaオブジェクト初期化
         self.pca = None
     def pca_fit(self,n=2):
         from sklearn.decomposition import PCA
         p = PCA(n_components=n)
-        return p.fit_transform(self.std_data)
+        return p.fit_transform(self.base)
         
     def explained_variance_ratio(self, n=[1]):
         #圧縮先の次元数を複数指定して、累積寄与率を比較する
@@ -26,7 +28,7 @@ class MyPCA:
         print(u"n,累積寄与率")
         for i in n:
             self.pca = PCA(n_components=i)
-            self.pca.fit(self.std_data)
+            self.pca.fit(self.base)
             kiyo = self.pca.explained_variance_ratio_
             #print("%d,%.6f" % (i,cumsum(kiyo)))
             print("%d,%.6f" % (i,cumsum(kiyo)[::-1][0]))
